1296874c3697f9f06f80f5834e4f25c6155abdaf
marcoswebermw <marcoswebermw@gmail.com>
2017-07-24 22:38:37 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='movie-rating-app',
+    packages=['app', 'test'],
+    test_suite='test',
+)
+
+# Para rodar os testes use "python setup.py test".
